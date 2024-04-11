c95f2ca7e0c2c84735ffcc673338039221da5af7
cristiansot <soluciongrafix@gmail.com>
2024-04-11 13:28:16 -0400

    Exercise 1.1 done

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./Header"
+import Content from "./Content"
+import Total from "./Total"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = (props) => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header course={course}  />
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+    </div>
   )
 }
 
-export default App
+export default App;
---
+++ part1/src/Content.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Content = (props) => {
+  return (
+    <>
+      <p>{props.part1} {props.exercises1}</p>
+      <p>{props.part2} {props.xercises2}</p>
+      <p>{props.part3} {props.exercises3}</p>
+    </>
+  )
+}
+
+export default Content;
